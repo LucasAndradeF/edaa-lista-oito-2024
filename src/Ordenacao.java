48b888a566e761e00f1c08032a75d6919537c206
Lucas Ferreira <lucasandradef44@gmail.com>
2024-04-17 22:14:19 -0300

    Add exercício Ordenação

---
+++ src/Ordenacao.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Ordenacao {
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<>();
+        try {
+            File file = new File(
+                    "C:\\Users\\Lucas Ferreira\\OneDrive\\Área de Trabalho\\edaa-lista-oito-2024\\src\\estudantes_una_2024.csv");
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                nomes.add(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (!nomes.isEmpty()) {
+            nomes.remove(0);
+        }
+        Collections.sort(nomes);
+
+        System.out.println("Ordenando Estudantes por Nome:");
+        for (String n : nomes) {
+            System.out.println("nome: " + n);
+        }
+    }
+}
